2c4e224beaa84c49b3976bd4195975df01a3059f
quinngm <maensivuq@gmail.com>
2024-07-15 18:52:34 -0400

    fix

--- components/FormSection.jsx
+++ components/FormSection.jsx
@@ -2,15 +2,13 @@ import Form from "./Form";
 
 export default function FormSection() {
   return (
-    <div className=' bg-gray-200 w-full flex flex-row gap-3 justify-between py-24 px-6'>
+    <div className=' bg-gray-200 w-full flex flex-row gap-3 justify-between py-24 px-10'>
       <div className=" w-2/6">
-        <h1 className='text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl'>
-          Data to enrich your online business
+        <h1 className='text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl'>
+          Preserving Our Community's Beauty, One Yard at a Time.
         </h1>
         <p className='mt-6 text-lg leading-8 text-gray-600'>
-          Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem
-          cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat
-          aliqua.
+          Choosing the right partner to enhance the value & enjoyment of your yard is a significant decision. We are grateful for the opportunity to earn your trust & business. 
         </p>
       </div>
       <Form /> 
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -4,12 +4,12 @@ export default function Nav({ name }) {
 
     return (
         <div className=" w-full flex flex-row justify-between py-6 bg-gray-300 px-6">
-            <button className=" p-3 bg-red-400">name</button>
+            <button className=" p-3 bg-blue-400">Winter Park Irrigation</button>
 
             <nav className=" flex flex-row gap-4">
-                <button className=" text-zinc-200 bg-gray-500 px-6 rounded-md">Hello</button>
-                <button className=" text-zinc-200 bg-gray-500 px-6 rounded-md">Hello</button>
-                <button className=" text-zinc-200 bg-gray-500 px-6 rounded-md">Hello</button>
+                <button className=" text-zinc-200 bg-gray-500 px-6 rounded-md">About Us</button>
+                <button className=" text-zinc-200 bg-gray-500 px-6 rounded-md">Products & Services</button>
+                <button className=" text-zinc-200 bg-gray-500 px-6 rounded-md">Schedule</button>
             </nav>
         </div>
     )
